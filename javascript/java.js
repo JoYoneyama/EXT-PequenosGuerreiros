215f07824e1027e8936256015f36e7f0b1b3aae7
JoYoneyama <jvsyoneyama@gmail.com>
2024-08-09 12:31:30 -0300

    slider nossos parceiros

--- javascript/java.js
+++ javascript/java.js
@@ -17,3 +17,26 @@ document.addEventListener("DOMContentLoaded", () => {         //checa se o docum
   allAnimatedElements.forEach((element) => observer.observe(element));      //adiciona o observer para todos esses elementos
 
 })
+
+//carrossel da seção parceiros
+
+const container = document.querySelector(".parceiros-content");
+const imagens = document.querySelectorAll(".parceiros-content img");
+
+let contador = 0;
+
+function slider(){
+
+  contador++;
+
+  if(contador > imagens.length - 1){
+
+    contador = 0;
+
+  }
+
+  container.style.transform = `translateX(${-contador * 198}px)`;  //-contador pq esta movendo a img da esquerda para a direita
+                                                                   
+}                                                                  //o contador multiplica esse valor para atravessar todas as imagens
+
+setInterval(slider, 2000);
